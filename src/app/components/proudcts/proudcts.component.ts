688db162d6297e89780973b302a020b4b635f70a
Zeyad-Balal <zeyad.hassan.balal@gmail.com>
2025-01-25 16:32:22 +0200

    importing assets, global style & creating main components

---
+++ src/app/components/proudcts/proudcts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proudcts',
+  standalone: true,
+  imports: [],
+  templateUrl: './proudcts.component.html',
+  styleUrl: './proudcts.component.scss'
+})
+export class ProudctsComponent {
+
+}
