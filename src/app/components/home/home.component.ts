645ed066a1ab04cd1d1d749441174330fac28486
Zeyad Hassan <zeyad.hassan.balal@gmail.com>
2025-01-29 12:49:19 +0200

    Display popular products in home page view with adjusting its style 🏠
    In feat/login branch:
    1.Product service implemented with API [/api/v1/products] => getAllProducts() & getSpecificProducts() methods
    2.Implemented Home component logic by retrieve product and display it on /home
    3.Generate IProduct interface to store responsed data from API
    4.Adjusting global file style => hover on product

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ProductsService } from '../../Core/services/products.service';
+import { IProduct } from '../../Core/interfaces/iproduct';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,23 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+private readonly _ProductService = inject(ProductsService);
+constructor() { }
+
+productList:IProduct[] = [];
+
+//to load products once the component is opened
+ngOnInit(): void {
+  this._ProductService.getAllProducts().subscribe({
+    next: (res) => {
+      this.productList = res.data;
+      console.log(res.data);
+    },
+    error: (err) => {
+      console.log(err);
+    },
+  });
+}
 
 }
